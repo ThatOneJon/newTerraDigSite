a60611c639bf0c7f3213ee7fd558ca7f4df7dcf8
thatOneJon <83345498+ThatOneJon@users.noreply.github.com>
2023-02-17 23:05:28 +0100

    update

--- src/pages/terraDigital.js
+++ src/pages/terraDigital.js
@@ -1,15 +1,28 @@
 import React from "react"
 import Layout from "../components/layout"
 import Container from "react-bootstrap/Container"
+import ReactMarkdown from 'react-markdown';
+import{graphql} from "gatsby"
 
 
-
-export default function TerraDigital() {
+export default function TerraDigital({data}) {
+    console.log(data)
     return(
         <Layout>
             <Container className="mt-3" style={{minHeight:"100vh"}}>
-                <p>Terra Digital</p>
+                <ReactMarkdown>
+                    {data.markdownRemark.rawMarkdownBody}
+                </ReactMarkdown>
             </Container>
         </Layout>
     )
 }
+
+export const query=  graphql`
+    query MyQuery {
+        markdownRemark(frontmatter: {slug: {eq: "terra-digital"}}) {
+        rawMarkdownBody
+        id
+        }
+    }
+`
